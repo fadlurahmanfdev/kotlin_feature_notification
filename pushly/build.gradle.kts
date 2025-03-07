f0f9a451382900d3c8bcbc58c59b9650f2fd0f2c
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2025-03-07 17:02:10 +0700

    feat(library): update gradle & github workflow

--- pushly/build.gradle.kts
+++ pushly/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("maven-publish")
+
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 android {
@@ -57,12 +61,45 @@ publishing {
     publications {
         register<MavenPublication>("release"){
             groupId = "com.github.fadlurahmanfdev"
-            artifactId = "kotlin_core_notification"
-            version = "0.0.11"
+            artifactId = "kotlin_feature_notification"
+            version = "0.0.1"
 
             afterEvaluate {
                 from(components["release"])
             }
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("com.fadlurahmanfdev", "pushly", "0.0.1")
+
+    pom {
+        name.set("Pushly Notification")
+        description.set("Library to simplify perform notification")
+        inceptionYear.set("2025")
+        url.set("https://github.com/fadlurahmanfdev/kotlin_feature_notification/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("fadlurahmanfdev")
+                name.set("Taufik Fadlurahman Fajari")
+                url.set("https://github.com/fadlurahmanfdev/")
+            }
+        }
+        scm {
+            url.set("https://github.com/fadlurahmanfdev/kotlin_feature_notification/")
+            connection.set("scm:git:git://github.com/fadlurahmanfdev/kotlin_feature_notification.git")
+            developerConnection.set("scm:git:ssh://git@github.com/fadlurahmanfdev/kotlin_feature_notification.git")
         }
     }
 }
